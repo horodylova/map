b6734efb74989701407d30327c864932d8092531
Svitlana Horodylova <horodylova.sv@gmail.com>
2025-04-30 10:51:34 +0100

    -added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
-    <link rel="icon" href="/map.svg" type="image/svg+xml" />
+    <link rel="icon" href="/logo.png" type="image/svg+xml" />
     </head>
     <body>
         <main>
